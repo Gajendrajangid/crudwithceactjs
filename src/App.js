5378b3a37baf2bb90646c69f688edf15f1b730b2
Gajendrajangid <47345234+Gajendrajangid@users.noreply.github.com>
2021-07-18 15:40:37 +0530

    sss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
- 
-import React from 'react'; 
-import { ToastContainer } from 'react-toastify'; 
+
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import Header from './Layout/Header'
 import Footer from './Layout/Footer'
 import Layout from './Layout/Layout'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';  
 function App() {
   return (
     <div className="App">
-       <ToastContainer /> 
-       <Header/>
-       <Layout/>
-       <Footer/>
+      <ToastContainer />
+        <Layout />
+      <Footer />
     </div>
   );
 }
-
 export default App;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import routes from '../routes'
+import Header from './Header'
+import {
+    BrowserRouter as Router,
+    Route, Switch, Redirect
+} from 'react-router-dom';
 const Layout = () => {
     return (
-        <div className="container">
+        <div>
+            <Router>
+                <Header />
+                <hr />
+                <div className="container">
+                    <Suspense fallback={<div>Loading... </div>}>
+                        <Switch>
+                            {routes.map((route, idx) => {
+                                return route.component ? (
+                                    <Route
+                                        key={idx}
+                                        path={route.path}
+                                        exact={route.exact}
+                                        name={route.name}
+                                        render={props => (
+                                            <route.component {...props} />
+                                        )} />
+                                ) : (null);
+                            })}
+                            <Redirect from="/" to="/home" />
+                        </Switch>
+                    </Suspense>
+                </div>
+
+                <hr />
+            </Router>
         </div>
     )
 }
